c2393fc17f6aa49d203b94ca9e3ce7584ccc619d
Subbu <msmb4u@gmail.com>
2018-07-28 07:35:42 +0530

    Init commit

---
+++ src/main/java/App.java
@@ -0,0 +1,13 @@
+
+public class App {
+
+	public App() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("This is my first maven application");
+
+	}
+
+}
